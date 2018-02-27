97e97eb8a81ba43f70e6320601d15a148a380504
benyaya225 <dao_benyaya@hotmail.fr>
2018-02-27 11:37:23 +0100

    michelin restaurants json

--- Michelin.js
+++ Michelin.js
@@ -1,9 +1,11 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-
-function restaurants(nbPage) {
-  var url = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-0"+nbPage;
+var obj = {table: []};
+var json = JSON.stringify(obj);
+function restaurants() {
+  for (var i = 0; i < 35; i++) {
+  var url = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-0"+i;
 
   request(url, function (error, response, html) {
     if (!error && response.statusCode == 200) {
@@ -11,13 +13,23 @@ function restaurants(nbPage) {
       var $ = cheerio.load(html);
       $(".poi-search-result").find(".poi_card-display-title").each(function(index, element){
         var a = $(element);
-        console.log(a.text().trim())
+        obj.table.push({name:a.text().trim()});
+        //console.log(obj)
+        json = JSON.stringify(obj);
+        var fs = require('fs');
+        fs.writeFile('restaurant.json', json, 'utf-8',(err) => {
+            if (err) {
+                console.error(err);
+                return;
+            };
+        });
       });
     }
   });
 }
+console.log(obj)
 
-var result;
-for (var i = 0; i < 35; i++) {
-  restaurants(i);
+
 }
+
+restaurants();
